8fa3cb14bf27d38c6514bf0f83e00417e0183c43
Gayoung Park <gayoungp@uw.edu>
2020-10-21 10:04:20 -0700

    wrote basic codes for generate climate models. Added a code to read Korean alphabet

---
+++ climate.R
@@ -0,0 +1,209 @@
+require("ncdf4")
+require("lattice")
+require("ggplot2")
+
+# you'll need to get this file from
+# https://figshare.com/articles/LateQuaternary_Environment_nc/12293345/3
+# and move to our raw_data folder
+file <- here::here("raw_data/LateQuaternary_Environment.nc")
+
+env_nc      <- ncdf4::nc_open(file)
+longitude   <- ncdf4::ncvar_get(env_nc, "longitude")
+latitude    <- ncdf4::ncvar_get(env_nc, "latitude")
+years       <- ncdf4::ncvar_get(env_nc, "time")
+months      <- ncdf4::ncvar_get(env_nc, "month")
+temperature <- ncdf4::ncvar_get(env_nc, "temperature")
+biome       <- ncdf4::ncvar_get(env_nc, "biome")
+ncdf4::nc_close(env_nc)
+
+mean_annual_temperature <- apply(temperature, c(1, 2, 4), mean)
+
+# korean archaeological sites
+library(tidyverse)
+library(here)
+
+site <-
+  readxl::read_excel(here("raw_data/location.xlsx"))
+
+# early period
+site_early <-
+  site %>%
+  select(site_name,
+         lat_dd,
+         long_dd,
+         elevation,
+         period) %>%
+  filter(period == "early") %>% 
+  rowwise()  %>%
+  mutate(lonID = which.min(abs(longitude - long_dd)),
+         latID = which.min(abs(latitude - lat_dd))) %>%
+  mutate(mean_annual_temperature = list(tibble(year = years,
+                                               mean_annual_temperature = mean_annual_temperature[lonID, latID, ]))) %>%
+  unnest(mean_annual_temperature) %>%
+  filter(between(year, -6450, -5450)) %>% 
+  filter(mean_annual_temperature >0 )
+
+# try to make boxplot
+mat_per_site_plot <-
+  ggplot(site_early) +
+  aes(y = mean_annual_temperature,
+      x = reorder(site_name,
+                  mean_annual_temperature)) +
+  geom_boxplot() +
+  coord_flip() +
+  xlab("") +
+  ylab("Mean annual temperature (MAT, °C)") +
+  theme_gray(base_family='Apple SD Gothic Neo')
+
+#scatter plot
+early <-ggplot(site_early) + 
+  aes(y = mean_annual_temperature,
+      x = reorder(site_name,
+                  mean_annual_temperature)) +
+  geom_point() +
+  coord_flip() +
+  xlab("") +
+  ylab("Mean annual temperature: mid (MAT, °C)") +
+  theme_gray(base_family='Apple SD Gothic Neo')
+
+
+###################
+
+# mid period
+site_mid <-
+  site %>%
+  select(site_name,
+         lat_dd,
+         long_dd,
+         elevation,
+         period) %>%
+  filter(period == "mid") %>% 
+  rowwise()  %>%
+  mutate(lonID = which.min(abs(longitude - long_dd)),
+         latID = which.min(abs(latitude - lat_dd))) %>%
+  mutate(mean_annual_temperature = list(tibble(year = years,
+                                               mean_annual_temperature = mean_annual_temperature[lonID, latID, ]))) %>%
+  unnest(mean_annual_temperature) %>%
+  filter(between(year, -5450, -4950))%>% 
+  filter(mean_annual_temperature >0 )
+
+mid_mat_time_series_plot <-
+  ggplot() +
+  geom_line(data = site_mid,
+            aes(-year,
+                mean_annual_temperature,
+                group = site_name),
+            colour = "grey90") +
+  geom_line(data = site_mid %>%
+              group_by(year) %>%
+              summarise(mean_mean_annual_temperature = mean(mean_annual_temperature,
+                                                            na.rm = TRUE)),
+            aes(-year,
+                mean_mean_annual_temperature),
+            size = 2) +
+  scale_x_continuous(labels = scales::comma) +
+  coord_cartesian(ylim = c(1, 12)) +
+  theme_gray(base_family='Apple SD Gothic Neo') +
+  labs(y = "Mean annual temperature (MAT, °C)",
+       x = "Year (BP)")
+
+# try to make boxplot
+mid_mat_per_site_plot <-
+  ggplot(site_mid) +
+  aes(y = mean_annual_temperature,
+      x = reorder(site_name,
+                  mean_annual_temperature)) +
+  geom_boxplot() +
+  coord_flip() +
+  xlab("") +
+  ylab("Mean annual temperature (MAT, °C)") +
+  theme_gray(base_family='Apple SD Gothic Neo')
+
+#scatter plot
+mid<- 
+ggplot(site_mid) +
+  aes(y = mean_annual_temperature,
+      x = reorder(site_name,
+                  mean_annual_temperature)) +
+  geom_point() +
+  coord_flip() +
+  xlab("") +
+  ylab("Mean annual temperature: mid (MAT, °C)") +
+  theme_gray(base_family='Apple SD Gothic Neo')
+
+# add elevation: not working due to the short time period
+ggplot() +
+  geom_line(data = site_mid,
+            aes(-year,
+                mean_annual_temperature,
+                group = site_name),
+            colour = "grey90") +
+  geom_line(data = site_mid %>%
+              group_by(year) %>%
+              summarise(mean_mean_annual_temperature = mean(mean_annual_temperature,
+                                                            na.rm = TRUE)),
+            aes(-year,
+                mean_mean_annual_temperature),
+            size = 2) +
+  scale_x_continuous(labels = scales::comma) +
+  coord_cartesian(ylim = c(1, 12)) +
+  theme_minimal() +
+  labs(y = "Mean annual temperature (MAT, °C)",
+       x = "Year (BP)")
+
+###################
+
+# late period
+site_late <-
+  site %>%
+  select(site_name,
+         lat_dd,
+         long_dd,
+         elevation,
+         period) %>%
+  filter(period == "late") %>% 
+  rowwise()  %>%
+  mutate(lonID = which.min(abs(longitude - long_dd)),
+         latID = which.min(abs(latitude - lat_dd))) %>%
+  mutate(mean_annual_temperature = list(tibble(year = years,
+                                               mean_annual_temperature = mean_annual_temperature[lonID, latID, ]))) %>%
+  unnest(mean_annual_temperature) %>%
+  filter(between(year, -4950, -3000))%>% 
+  filter(mean_annual_temperature >0 )
+
+
+# try to make boxplot
+late_mat_per_site_plot <-
+  ggplot(site_late) +
+  aes(y = mean_annual_temperature,
+      x = reorder(site_name,
+                  mean_annual_temperature)) +
+  geom_boxplot() +
+  coord_flip() +
+  xlab("") +
+  ylab("Mean annual temperature: late (MAT, °C)") +
+  theme_gray(base_family='Apple SD Gothic Neo')
+
+#scatter plot
+late <- ggplot(site_late) +
+  aes(y = mean_annual_temperature,
+      x = reorder(site_name,
+                  mean_annual_temperature)) +
+  geom_point() +
+  coord_flip() +
+  xlab("") +
+  ylab("Mean annual temperature: late (MAT, °C)") +
+  theme_gray(base_family='Apple SD Gothic Neo')
+
+
+
+
+#----------------------------------------------------------------------
+
+# combine plots
+library(cowplot)
+
+plot_grid(early,
+          mid,
+          late,
+          ncol = 1)
